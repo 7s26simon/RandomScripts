30b7033224b47bc29cf77d8232f071663e867bf3
Simon McCabe <5106638+smc13@users.noreply.github.com>
2019-05-27 14:35:36 +0100

    Update odds.py
    Fixed calculations
    Fixed formatting
    Added breakdown of bet

--- odds.py
+++ odds.py
@@ -3,17 +3,17 @@ import click
 def odds ():
     a = ("What were the fractional odds you bet on? (E.g if 5/4, type '5' enter, '4' enter)")
     print(a)
-    y = int(input())
-    z = int(input())
+    y = int(input()) # 8
+    z = int(input()) # 1
     print ("What did you bet? E.g for £20, type '20' followed by enter")
-    yourBet = int(input())
-    winnings = (z * y) + z
+    yourBet = int(input()) # 10
+    winnings = (yourBet/z) * (y) + yourBet
     if click.confirm(("\nSo the fractional odds were: %d/%d? and you bet £%d" %(y,z,yourBet)), default=True):
-        print("\n")
-        print ("\nYour winnings would be:", "£"+str(winnings)+"\n")
-        print("Good luck, and thank you for using this script!")
+        print ("\nYour winnings would be:", "£"+str(winnings))
+        sum = winnings-yourBet
+        print("Breakdown... You bet: [£%d], Actual profit/winnings: [£%d] at odds of %d/%d" %(yourBet, sum, y,z))
+        print("\nGood luck, and thank you for using this script!")
     else:
         print("\nRestarting script...")
         odds()
-
 odds()
